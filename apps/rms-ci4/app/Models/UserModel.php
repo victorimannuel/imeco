490322af2979028022e167e343ab3d33982c7221
victorimannuel <ops@imannuelvictor.com>
2026-01-26 17:21:49 +0700

    refactor: remove rms-ci4 to later be added as a submodule

--- apps/rms-ci4/app/Models/UserModel.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace App\Models;
-use CodeIgniter\Model;
-
-class UserModel extends Model
-{
-    protected $table = 'users';
-    protected $allowedFields = ['nama', 'email', 'password_hash'];
-}
--- apps/rms-ci4/app/Views/main_page.php
+++
@@ -1,8 +0,0 @@
-<?= $this->extend('layouts/template') ?>
-
-<?= $this->section('content') ?>
-
-    <h2>Selamat Datang!</h2>
-    <p>Ini adalah konten untuk halaman utama.</p>
-
-<?= $this->endSection() ?>
